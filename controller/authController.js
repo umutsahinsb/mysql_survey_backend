3fe209eeb542f1f9d214d2c47c980b60983d9750
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-08 00:21:23 +0300

    x

--- controller/authController.js
+++ controller/authController.js
@@ -272,6 +272,7 @@ const authController ={
     
             // Get the newly inserted task's ID
             const taskIdQuery = "SELECT * FROM iş ORDER BY is_id DESC LIMIT 1;";
+            console.log(taskIdQuery);
 
             // Update the pollster's yapilacak_is field
             const updatePollsterQuery = "UPDATE anketör SET yapilacak_is = ? WHERE kullanici_id = ?";
