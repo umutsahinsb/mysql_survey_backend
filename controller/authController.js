97c1a7b0a16aff6c7d398717dac83c44fb09a809
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-08 02:23:38 +0300

    login ekranında anketör için gerekli düzenlemeler

--- controller/authController.js
+++ controller/authController.js
@@ -115,12 +115,12 @@ const authController ={
             const check = await bcrypt.compare(password, hash);
     
             if (check) {
-                const cityQuery = "SELECT iller.il_adi FROM iller JOIN konum ON iller.il_id = konum.il_id WHERE konum.konum_id = ?";
+                const cityQuery = "SELECT il_adi FROM iller JOIN konum ON iller.il_id = konum.il_id WHERE konum.konum_id = ?";
                 const [cityResult,] = await pool.query(cityQuery, [konum_id]);
                 const city = cityResult[0].city;
                 console.log(city);
 
-                const districtQuery = "SELECT konum.ilçe FROM konum WHERE konum.konum_id = ?";
+                const districtQuery = "SELECT ilçe FROM konum WHERE konum_id = ?";
                 const [districtResult,] = await pool.query(districtQuery, [konum_id]);
                 const district = districtResult[0].district;
                 console.log(district);
