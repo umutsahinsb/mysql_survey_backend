cdf571106f0da2098f0093bd7e2dba6c6f7fec5f
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-08 01:53:52 +0300

    login ekranında anketör için gerekli düzenlemeler

--- controller/authController.js
+++ controller/authController.js
@@ -140,7 +140,7 @@ const authController ={
                 console.log(result[0]);
 
                 // Anketör verilerini getPollsterData fonksiyonuyla birleştir
-                const pollsterData = getPollsterData(title, startDate, endDate, city, district, template, percentageOfWomen);
+                const pollsterData = getPollsterData(result[0].title, startDate, endDate, city, district, template, percentageOfWomen);
 
 
                 let userData = {"userData":getUserData(kullanici_id, rol, isim, soyisim, telefon, dogumtarihi, cinsiyet, city, email)};
