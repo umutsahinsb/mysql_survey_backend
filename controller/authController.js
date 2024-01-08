160e92e4719b97c38a1890427427e54e97ef7319
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-08 23:01:18 +0300

    iş bilgileri alma fix

--- controller/authController.js
+++ controller/authController.js
@@ -27,25 +27,6 @@ function getUserData(
   };
 }
 
-async function getPollsterUserId(taskId) {
-    const pollsterQuery = `
-      SELECT anketör.kullanici_id
-      FROM anketör
-      JOIN iş ON anketör.yapilacak_is = iş.is_id
-      WHERE iş.is_id = ?
-    `;
-  
-    const [pollsterResult] = await pool.query(pollsterQuery, [taskId]);
-  
-    if (pollsterResult.length > 0) {
-      const pollsterUserId = pollsterResult[0].kullanici_id;
-      console.log(pollsterUserId);
-      // İşlemlerinizi devam ettirin
-    } else {
-      console.log("Belirtilen task ID'si ile eşleşen anketör bulunamadı.");
-      // Hata durumu veya mesajınıza göre işlemlerinizi devam ettirin
-    }
-  }
 
 function getPollsterData(
   title,
@@ -594,9 +575,20 @@ const authController = {
         
         console.log(taskResult);
        
-         const pollsterUserId = getPollsterUserId(taskId);
-         console.log(pollsterUserId);
-          const pollsterName = getUserName(pollsterUserId);
+        const pollsterQuery = `
+        SELECT anketör.kullanici_id
+        FROM anketör
+        JOIN iş ON anketör.yapilacak_is = iş.is_id
+        WHERE iş.is_id = ?
+        `;
+    
+      const [pollsterResult] = await pool.query(pollsterQuery, [taskId]);
+    
+    
+        const pollsterUserId = pollsterResult[0].kullanici_id;
+      
+        console.log(pollsterUserId);
+        const pollsterName = getUserName(pollsterUserId);
 
         const locationQuery = `
           SELECT
