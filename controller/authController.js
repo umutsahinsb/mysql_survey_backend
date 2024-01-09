ef34afae06516662a0fc8d73a5ff04550413dd46
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-09 20:26:18 +0300

    anket oluşturma

--- controller/authController.js
+++ controller/authController.js
@@ -240,7 +240,7 @@ const authController = {
         console.log(district);
 
         // Anketör ve İş bilgilerini birleştir
-        const query = `CALL GetAnketorAndIsInfo(?)`;
+        const query = `CALL GetAnketorAndIs(?)`;
         const [result] = await pool.query(query, [kullanici_id]);
 
         if (result.length > 0) {
