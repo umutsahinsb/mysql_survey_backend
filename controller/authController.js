292a0be48c1449a7dcd2a3e140fdbb2fb6c24e15
Umut Şahin <152120201019@ogrenci.ogu.edu.tr>
2024-01-09 20:59:35 +0300

    anket oluşturma

--- controller/authController.js
+++ controller/authController.js
@@ -233,16 +233,15 @@ const authController = {
         const [cityResult] = await pool.query(cityQuery, [konum_id]);
         const city = cityResult[0].il_adi;
         console.log(city);
-
         const districtQuery = "SELECT ilçe FROM konum WHERE konum_id = ?";
         const [districtResult] = await pool.query(districtQuery, [konum_id]);
         const district = districtResult[0].ilçe;
         console.log(district);
-
         // Anketör ve İş bilgilerini birleştir
-        const query = `CALL GetAnketorAndIs(?)`;
+        const query = `
+                        SELECT anketör.*, iş.is_id, iş.is_basligi, iş.baslangic_tarihi, iş.bitis_tarihi, 
+                        iş.belirlenen_sablon, iş.kadin_orani FROM anketör LEFT JOIN iş ON anketör.yapilacak_is = iş.is_id WHERE anketör.kullanici_id = ?`;
         const [result] = await pool.query(query, [kullanici_id]);
-        console.log(result);
         
         if (result.length > 0) {
           const { taskId, title, startDate, endDate, template, percentageOfWoman } =
@@ -250,14 +249,14 @@ const authController = {
           console.log(result[0]);
           // Anketör verilerini getPollsterData fonksiyonuyla birleştir
           const pollsterData = getPollsterData(
-            result[0].taskId,
-            result[0].title,
-            result[0].startDate,
-            result[0].endDate,
+            result[0].is_id,
+            result[0].is_basligi,
+            result[0].baslangic_tarihi,
+            result[0].bitis_tarihi,
             city,
             district,
-            result[0].template,
-            result[0].percentageOfWoman
+            result[0].belirlenen_sablon,
+            result[0].kadin_orani
           );
           console.log("Pollster Data:", pollsterData);
           let userData = {
